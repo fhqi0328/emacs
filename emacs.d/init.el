3375e47c7e012b8db7f30af7f6845a0b8a6a8bc4
zhaojingzhen <zhaojingzhen@megvii.com>
2023-07-26 17:36:07 +0800

    update config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -62,6 +62,15 @@
 ;;   :ensure t
 ;;   :config (load-theme 'gruvbox-dark-hard t))
 
+(use-package solarized-theme
+  ;; color theme
+  :ensure t
+  :config (load-theme 'solarized-gruvbox-dark t))
+
+(use-package magit
+  ;; git package
+  :ensure t)
+
 
 ;;;;;;;; 基础设置
 ;; 关闭文件自动备份
@@ -90,8 +99,16 @@
       scroll-margin 1)
 
 ;; 使用y/n 代替yes/no
-(defalias 'y-or-n-p 'yes-or-no-p)
+(fset 'y-or-n-p 'yes-or-no-p)
 
+;; 光标靠近鼠标时, 指针自动让开
+(mouse-avoidance-mode 'animate)
+
+;; C-k删除当前位置到行未内容
+(setq-default kill-whole-line t)
+
+;; 设置默认shell
+(setq-default shell-file-name "/bin/bash")
 
 
 ;;;;;;; 外观设置
@@ -122,11 +139,11 @@
 (display-time-mode t)
 
 ;; 主题设置
-(load-theme 'wombat t)
+;; (load-theme 'wombat t)
 
 ;; 显示列线
-(setq display-fill-column-indicator-column 120)
-(global-display-fill-column-indicator-mode t)
+;; (setq display-fill-column-indicator-column 120)
+;; (global-display-fill-column-indicator-mode t)
 
 
 ;;;;;;; 编程设置
@@ -170,13 +187,16 @@
 (global-set-key (kbd "<f1>") 'other-window)
 
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" default))
  '(package-selected-packages
-   '(use-package neotree monokai-theme ivy-file-preview gruvbox-theme goto-chg go-mode flycheck counsel company)))
+   '(magit use-package neotree monokai-theme ivy-file-preview gruvbox-theme goto-chg go-mode flycheck counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
