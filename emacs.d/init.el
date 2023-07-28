2bafc11d5a573787c58dddfa92310f4bf4d6db0d
zhaojingzhen <zhaojingzhen@megvii.com>
2023-07-28 09:12:40 +0800

    update color scheme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,10 +21,12 @@
 	use-package-expand-minimally t))
   
 (use-package neotree
+  ;; like NERDTree in Vim
   :ensure t
   :bind ("<f2>" . neotree-toggle))
 
 (use-package go-mode
+  ;; support go
   :ensure t
   :mode "\\.go\\'")
 
@@ -57,19 +59,15 @@
   :hook ((prog-mode . company-mode)
 	 (text-mode . company-mode)))
 
-;; (use-package gruvbox-theme
-;;   ;; color theme
-;;   :ensure t
-;;   :config (load-theme 'gruvbox-dark-hard t))
-
-(use-package solarized-theme
+(use-package gruvbox-theme
   ;; color theme
   :ensure t
-  :config (load-theme 'solarized-gruvbox-dark t))
+  :config (load-theme 'gruvbox-dark-hard t))
 
-(use-package magit
-  ;; git package
-  :ensure t)
+;; (use-package solarized-theme
+;;   ;; color theme
+;;   :ensure t
+;;   :config (load-theme 'solarized-gruvbox-dark t))
 
 
 ;;;;;;;; 基础设置
@@ -183,10 +181,6 @@
 ;; 使用C-o切换窗口
 (global-set-key (kbd "C-o") 'other-window)
 
-;; 使用<F1>切换窗口
-(global-set-key (kbd "<f1>") 'other-window)
-
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -196,7 +190,7 @@
  '(custom-safe-themes
    '("3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" default))
  '(package-selected-packages
-   '(magit use-package neotree monokai-theme ivy-file-preview gruvbox-theme goto-chg go-mode flycheck counsel company)))
+   '(use-package neotree monokai-theme ivy-file-preview gruvbox-theme goto-chg go-mode counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
