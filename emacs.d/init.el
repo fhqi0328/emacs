917a5afec859fc273d02c473e8b4469c5a3cf5d0
zhaojingzhen <zhaojingzhen@megvii.com>
2023-07-28 11:48:58 +0800

    update evil config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -83,6 +83,7 @@
     (kbd "RET") 'xref-goto-xref)
   (define-key evil-motion-state-map (kbd "RET") 'Buffer-menu-this-window)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-normal-state-map (kbd "?") 'swiper-isearch-backward)
   (define-key evil-normal-state-map (kbd "/") 'swiper-isearch))
 
 
@@ -204,7 +205,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" default))
  '(package-selected-packages
    '(evil use-package neotree monokai-theme ivy-file-preview gruvbox-theme goto-chg go-mode counsel company)))
 (custom-set-faces
