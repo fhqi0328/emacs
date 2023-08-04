7dd91755f5d53ef85c2f6b47d63d7cf81734a56d
zhaojingzhen <zhaojingzhen@megvii.com>
2023-08-04 13:46:03 +0800

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,7 +6,6 @@
 (prefer-coding-system 'utf-8)
 
 
-
 ;;;;;;; 插件安装
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -16,6 +15,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (eval-and-compile
   (setq use-package-always-ensure t
 	use-package-expand-minimally t))
@@ -23,26 +23,33 @@
 (use-package neotree
   ;; like NERDTree in Vim
   :ensure t
+  ;; load after 1 second of idle time
+  :defer 1
   :bind ("<f2>" . neotree-toggle))
 
 (use-package go-mode
   ;; support go
   :ensure t
+  :defer 1
   :mode "\\.go\\'"
   :init (add-hook 'before-save-hook #'gofmt-before-save))
 
 (use-package swiper
   ;; comment: for ivy
-  :ensure t)
+  :ensure t
+  :defer 1)
 
 (use-package ivy-file-preview
   ;; comment: for ivy
-  :ensure t)
+  :ensure t
+  :defer 1)
 
 (use-package ivy
   :ensure t
+  :defer 1
   :bind (("C-s" . swiper-isearch)
 	 ("C-r" . swiper-isearch-backward)
+	 ("C-x C-f" . counsel-find-file)
 	 ("C-x b" . ivy-switch-buffer))
   :init (setq ivy-count-format "(%d/%d) "
               ivy-use-virtual-buffers t
@@ -51,6 +58,7 @@
 
 (use-package company
   :ensure t
+  :defer 1
   :commands company-mode
   :init (setq company-minimum-prefix-length 1
 	      company-tooltip-align-annotations t
@@ -60,32 +68,16 @@
   :hook ((prog-mode . company-mode)
 	 (text-mode . company-mode)))
 
-(use-package gruvbox-theme
-  ;; color theme
+;; (use-package gruvbox-theme
+;;   ;; color theme
+;;   :ensure t
+;;   :config (load-theme 'gruvbox-dark-hard t))
+
+
+(use-package monokai-theme
+  ;; monokai color theme
   :ensure t
-  :config (load-theme 'gruvbox-dark-hard t))
-
-;; (use-package evil-collection
-;;   ;; a key-collection for evil mode
-;;   :ensure t
-;;   :init (setq evil-want-keybinding nil))
-
-;; (use-package evil
-;;   ;; add vim mode for Emacs
-;;   :ensure t
-;;   :init
-;;   (setq evil-want-C-u-scroll t)
-;;   (setq evil-want-keybinding nil)
-;;   :config
-;;   (evil-mode 1)
-;;   (evil-set-initial-state 'xref--xref-buffer-mode 'normal)
-;;   (evil-collection-set-readonly-bindings 'xref--xref-buffer-mode-map)
-;;   (evil-collection-define-key 'normal 'xref--xref-buffer-mode-map
-;;     (kbd "RET") 'xref-goto-xref)
-;;   (define-key evil-motion-state-map (kbd "RET") 'Buffer-menu-this-window)
-;;   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-;;   (define-key evil-normal-state-map (kbd "?") 'swiper-isearch-backward)
-;;   (define-key evil-normal-state-map (kbd "/") 'swiper-isearch))
+  :config (load-theme 'monokai t))
 
 
 ;;;;;;;; 基础设置
@@ -94,6 +86,9 @@
 
 ;; 自动加载
 (global-auto-revert-mode t)
+
+;; 支持高亮当前单词
+(global-hi-lock-mode t)
 
 ;; 保存时删除多余空白字符
 (add-hook 'before-save-hook 'delete-trailing-withspace)
@@ -111,20 +106,21 @@
 (setq auto-save-list-file-prefix nil)
 
 ;; 连续滚动
-(setq scroll-step 1
-      scroll-margin 1)
+(setq scroll-step 1)
+(setq scroll-margin 1)
 
 ;; 使用y/n 代替yes/no
 (fset 'y-or-n-p 'yes-or-no-p)
 
-;; 光标靠近鼠标时, 指针自动让开
+;; 光标靠近鼠标时, 指针自动让开(GUI下)
 (mouse-avoidance-mode 'animate)
-
-;; C-k删除当前位置到行未内容
-;; (setq-default kill-whole-line t)
 
 ;; 设置默认shell
 (setq-default shell-file-name "/bin/bash")
+
+;; 设置垃圾回收阈值，提升启动速度
+(setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 2 1000 1000))
 
 
 ;;;;;;; 外观设置
@@ -156,6 +152,7 @@
 
 ;; 主题设置
 ;; (load-theme 'wombat t)
+;; (load-theme 'leuven t)
 
 ;; 显示列线
 ;; 通过set-fill-column指定, 快捷键C-x f
@@ -173,24 +170,11 @@
 
 
 ;;;;;;; 快捷键设置
-;; sh-mode-map下的快捷键
-(with-eval-after-load "sh-script"
-  (define-key sh-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
-  (define-key sh-mode-map (kbd "C-c C-s") 'project-find-regexp)
-  (define-key sh-mode-map (kbd "C-c C-f") 'project-find-file))
-
 ;; 使用ibuffer替换原buffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; M-g 跳转到指定行
 (global-set-key (kbd "M-g") 'goto-line)
-
-;; 使用C-c C-c注释/取消注释
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
-;; 使用C-c C-s在项目中正则搜索文本
-(global-set-key (kbd "C-c C-s") 'project-find-regexp)
-;; 使用C-c C-f在项目中搜索文件
-(global-set-key (kbd "C-c C-f") 'project-find-file)
 
 ;; 使用C-2或C-@ 进行set mark
 (global-set-key (kbd "C-2") 'set-mark-command)
@@ -201,6 +185,14 @@
 ;; 使用ctrl-<backspace> to kill word, not working when ssh
 (global-set-key (kbd "C-<DEL>") 'backward-kill-word)
 
+;; 使用<F8>注释/取消注释
+(global-set-key (kbd "<f8>") 'comment-or-uncomment-region)
+
+;; 使用Ctrl-x p s => project-find-regexp
+(global-set-key (kbd "C-x p s") 'project-find-regexp)
+;; 使用Ctrl-x p f => project-find-file
+(global-set-key (kbd "C-x p f") 'project-find-file)
+
 
 
 (custom-set-variables
@@ -209,7 +201,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil use-package neotree ivy-file-preview gruvbox-theme goto-chg go-mode counsel company)))
+   '(monokai-theme use-package neotree ivy-file-preview gruvbox-theme goto-chg go-mode counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
