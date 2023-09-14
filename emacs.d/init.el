3bdf6828948a34244965f1770e9381e77039abd9
zhaojingzhen <zhaojingzhen@megvii.com>
2023-09-14 09:59:44 +0800

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/my/")
 (require 'init-basic)
-(require 'init-plugin)
 (require 'init-ui)
+(require 'init-plugin)
 (require 'init-program)
 (require 'init-hotkey)
 
@@ -11,7 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet use-package pyvenv pkg-info neotree magit-section ivy-file-preview hl-todo highlight-indentation gruvbox-theme goto-chg go-mode git-commit counsel company annalist)))
+   '(yasnippet use-package pkg-info ivy-file-preview hl-todo highlight-indentation gruvbox-theme goto-chg go-mode git-commit counsel company annalist)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/my/init-plugin.el
+++ emacs.d/my/init-plugin.el
@@ -14,14 +14,7 @@
 
 (eval-and-compile
   (setq use-package-always-ensure t
-	use-package-expand-minimally t))
-
-(use-package neotree
-  ;; like NERDTree in Vim
-  :ensure t
-  ;; load after 1 second of idle time
-  :defer 1
-  :bind ("<f2>" . neotree-toggle))
+        use-package-expand-minimally t))
 
 (use-package go-mode
   ;; support go
@@ -30,12 +23,12 @@
   :mode "\\.go\\'"
   :init (add-hook 'before-save-hook #'gofmt-before-save))
 
-(use-package counsel
+(use-package swiper
+  ;; comment: for ivy
   :ensure t
   :defer 1)
 
-(use-package swiper
-  ;; comment: for ivy
+(use-package counsel
   :ensure t
   :defer 1)
 
@@ -48,13 +41,13 @@
   :ensure t
   :defer 1
   :bind (("C-s" . swiper-isearch)
-	 ("C-r" . swiper-isearch-backward)
-	 ("C-x C-f" . counsel-find-file)
-	 ("C-x b" . ivy-switch-buffer)
-	 ("C-M-j" . ivy-immediate-done))
+         ("C-r" . swiper-isearch-backward)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x b" . ivy-switch-buffer)
+         ("C-M-j" . ivy-immediate-done))
   :init (setq ivy-count-format "(%d/%d) "
-	      ivy-use-virtual-buffers t
-	      enable-recursive-minibuffers t)
+              ivy-use-virtual-buffers t
+              enable-recursive-minibuffers t)
   :config (ivy-mode 1))
 
 (use-package company
@@ -62,14 +55,14 @@
   :defer 1
   :commands company-mode
   :init (setq company-minimum-prefix-length 1
-	      company-tooltip-align-annotations t
-	      company-idle-delay 0.0
-	      company-show-numbers t
-	      company-selection-wrap-around t)
+              company-tooltip-align-annotations t
+              company-idle-delay 0.0
+              company-show-numbers t
+              company-selection-wrap-around t)
   :hook ((prog-mode . company-mode)
-	 (text-mode . company-mode)
-	 (c-mode-hook . company-mode)
-	 (c++-mode-hook . company-mode)))
+         (text-mode . company-mode)
+         (c-mode-hook . company-mode)
+         (c++-mode-hook . company-mode)))
 
 (use-package gruvbox-theme
   ;; color theme
@@ -92,35 +85,11 @@
 
 (use-package projectile
   :ensure t
-  :init  (projectile-mode +1)
+  :init          (projectile-mode +1)
   :config (setq projectile-project-search-path '(("~/go/src/" . 2) "~/Documents/src/"))
   :bind (:map projectile-mode-map
-	      ("C-c p" . projectile-command-map)
-	      ("M-p" . projectile-command-map)))
+              ("C-c p" . projectile-command-map)))
 
-
-;; (use-package evil-collection
-;;   ;; a key-collection for evil mode
-;;   :ensure t
-;;   :init (setq evil-want-keybinding nil))
-
-
-;; (use-package evil
-;;   ;; add vim mode for Emacs
-;;   :ensure t
-;;   :init
-;;   (setq evil-want-C-u-scroll t)
-;;   (setq evil-want-keybinding nil)
-;;   :config
-;;   (evil-mode 1)
-;;   (evil-set-initial-state 'xref--xref-buffer-mode 'emacs)
-;;   (evil-collection-set-readonly-bindings 'xref--xref-buffer-mode-map)
-;;   (evil-collection-define-key 'normal 'xref--xref-buffer-mode-map
-;;     (kbd "RET") 'xref-goto-xref)
-;;   (define-key evil-motion-state-map (kbd "RET") 'Buffer-menu-this-window)
-;;   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-;;   (define-key evil-normal-state-map (kbd "?") 'swiper-isearch-backward)
-;;   (define-key evil-normal-state-map (kbd "/") 'swiper-isearch))
 
 
 ;;;;;;;;;;;;;;;;;;;
